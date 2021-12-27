803efc4e13848648d0b75924111c8824120d09d1
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2021-12-27 18:08:50 +0530

    initial commit

---
+++ src/pages/CheckoutPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+import { PageHero, StripeCheckout } from '../components'
+// extra imports
+import { useCartContext } from '../context/cart_context'
+import { Link } from 'react-router-dom'
+
+const CheckoutPage = () => {
+  return <h4>checkout page</h4>
+}
+const Wrapper = styled.div``
+export default CheckoutPage
