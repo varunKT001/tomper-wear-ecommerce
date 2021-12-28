05485785851ebcc4258ec719d039a160c1575c7e
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2021-12-28 22:24:01 +0530

    hero, services, and contact ui done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,28 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 const Contact = () => {
-  return <h4>contact section</h4>
-}
+  return (
+    <Wrapper>
+      <div className='section-center'>
+        <h3>Join newsletter and get 20% off</h3>
+        <div className='content'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro dicta similique iste
+            reiciendis voluptates eius sit, repellendus beatae reprehenderit cum sequi vero officiis
+            labore unde placeat dolores enim perferendis veritatis.
+          </p>
+          <form className='contact-form'>
+            <input type='email' className='form-input' placeholder='enter your email' />
+            <button type='submit' className='submit-btn'>
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 const Wrapper = styled.section`
   padding: 5rem 0;
   h3 {
@@ -67,6 +86,6 @@ const Wrapper = styled.section`
   @media (min-width: 1280px) {
     padding: 15rem 0;
   }
-`
+`;
 
-export default Contact
+export default Contact;
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,10 +1,37 @@
-import React from 'react'
-import styled from 'styled-components'
-import { services } from '../utils/constants'
+import React from 'react';
+import styled from 'styled-components';
+import { services } from '../utils/constants';
 
 const Services = () => {
-  return <h4>services </h4>
-}
+  return (
+    <Wrapper>
+      <div className='section-center'>
+        <article className='header'>
+          <h3>
+            Custom furniture <br />
+            Built only for you
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus natus officia eaque
+            sed optio neque ipsa quod possimus, in ad?
+          </p>
+        </article>
+        <div className='services-center'>
+          {services.map((service) => {
+            const { id, icon, title, text } = service;
+            return (
+              <article key={id} className='service'>
+                <span className='icon'>{icon}</span>
+                <h4>{title}</h4>
+                <p>{text}</p>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   h3,
@@ -68,5 +95,5 @@ const Wrapper = styled.section`
       transform: translateY(5rem);
     }
   }
-`
-export default Services
+`;
+export default Services;
