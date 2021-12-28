1bf134cd8aa52f2eae54387a3e4b615fcd8b77ef
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2021-12-28 20:25:25 +0530

    error and about page ui done

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero, StripeCheckout } from '../components'
+import React from 'react';
+import styled from 'styled-components';
+import { PageHero, StripeCheckout } from '../components';
 // extra imports
-import { useCartContext } from '../context/cart_context'
-import { Link } from 'react-router-dom'
+import { useCartContext } from '../context/cart_context';
+import { Link } from 'react-router-dom';
 
 const CheckoutPage = () => {
-  return <h4>checkout page</h4>
-}
-const Wrapper = styled.div``
-export default CheckoutPage
+  return (
+    <main>
+      <PageHero title='checkout' />
+      <Wrapper className='page'>
+        <h3>Checkout</h3>
+      </Wrapper>
+    </main>
+  );
+};
+const Wrapper = styled.div``;
+export default CheckoutPage;
