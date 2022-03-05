2cdd3e5e43b39157f548fd84d16e2f404c12e7e4
Sagnikpal <74464019+sagnik2001@users.noreply.github.com>
2022-03-05 21:04:41 +0530

    Footer Css Enhancement

--- src/utils/init-firebase.js
+++ src/utils/init-firebase.js
@@ -4,7 +4,7 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'process.env.REACT_APP_API_KEY',
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
