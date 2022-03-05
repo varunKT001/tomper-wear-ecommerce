18e91190acf3f73e2bb72dd928e2b635cf5650e1
Shubham29012001 <smmaurya29012001@gmail.com>
2022-03-05 21:05:12 +0530

    Fix Shipping Bug for Checking Zip Code

--- src/components/ShippingForm/index.js
+++ src/components/ShippingForm/index.js
@@ -23,6 +23,7 @@ function ShippingForm({ confirmShipping }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const zipRegex = new RegExp('^[1-9][0-9]{5}$');
 
     if (!name) {
       return toast.error('Enter your Name');
@@ -45,7 +46,9 @@ function ShippingForm({ confirmShipping }) {
     if (!country) {
       return toast.error('Enter your Country');
     }
-
+    if (!zipRegex.test(postal_code)) {
+      return toast.error('Enter Valid Zip Code');
+    }
     return confirmShipping();
   };
 
@@ -108,7 +111,7 @@ function ShippingForm({ confirmShipping }) {
           {/* address postal code */}
           <div className='form-control'>
             <input
-              type='text'
+              type='number'
               name='postal_code'
               className='input'
               placeholder='Zip Code'
