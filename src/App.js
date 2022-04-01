af8095a6deb17a61a428ea3e91508a9b4a729ccf
Prikshit-anthal <prikshit_12013102@nitkkr.ac.in>
2022-04-01 12:11:21 +0530

    used properties maxheight and overflow to hide scrollbar when navbar opened

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar, Sidebar, Footer, Toast, ErrorBoundary } from './components';
+import { useProductsContext } from './context/products_context';
 import 'react-toastify/dist/ReactToastify.css';
 import {
   Home,
@@ -20,54 +21,59 @@ import {
 } from './pages';
 
 function App() {
+
+  const { isSidebarOpen } = useProductsContext();
+  const overflowPropertyToHideScroll=isSidebarOpen===true?'hidden':'scroll'
   return (
-    <Router>
-      <Toast />
-      <Navbar />
-      <Sidebar />
-      <ErrorBoundary>
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route exact path='/about'>
-            <About />
-          </Route>
-          <Route exact path='/products'>
-            <Products />
-          </Route>
-          <Route exact path='/cart'>
-            <Cart />
-          </Route>
-          <PrivateRoute exact path='/login'>
-            <Login />
-          </PrivateRoute>
-          <PrivateRoute exact path='/register'>
-            <Register />
-          </PrivateRoute>
-          <PrivateRoute exact path='/forgot-password'>
-            <Forgot />
-          </PrivateRoute>
-          <PrivateRoute exact path='/reset-password'>
-            <Reset />
-          </PrivateRoute>
-          <Route exact path='/products/:id' children={<SingleProduct />} />
-          <PrivateRoute exact path='/checkout'>
-            <Checkout />
-          </PrivateRoute>
-          <PrivateRoute exact path='/orders'>
-            <OrdersPage />
-          </PrivateRoute>
-          <PrivateRoute exact path='/profile'>
-            <ProfilePage />
-          </PrivateRoute>
-          <Route exact path='*'>
-            <Error />
-          </Route>
-        </Switch>
-      </ErrorBoundary>
-      <Footer />
-    </Router>
+    <div style={{maxHeight:'100vh',overflow:overflowPropertyToHideScroll}}>
+      <Router>
+        <Toast />
+        <Navbar />
+        <Sidebar />
+        <ErrorBoundary>
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route exact path='/about'>
+              <About />
+            </Route>
+            <Route exact path='/products'>
+              <Products />
+            </Route>
+            <Route exact path='/cart'>
+              <Cart />
+            </Route>
+            <PrivateRoute exact path='/login'>
+              <Login />
+            </PrivateRoute>
+            <PrivateRoute exact path='/register'>
+              <Register />
+            </PrivateRoute>
+            <PrivateRoute exact path='/forgot-password'>
+              <Forgot />
+            </PrivateRoute>
+            <PrivateRoute exact path='/reset-password'>
+              <Reset />
+            </PrivateRoute>
+            <Route exact path='/products/:id' children={<SingleProduct />} />
+            <PrivateRoute exact path='/checkout'>
+              <Checkout />
+            </PrivateRoute>
+            <PrivateRoute exact path='/orders'>
+              <OrdersPage />
+            </PrivateRoute>
+            <PrivateRoute exact path='/profile'>
+              <ProfilePage />
+            </PrivateRoute>
+            <Route exact path='*'>
+              <Error />
+            </Route>
+          </Switch>
+        </ErrorBoundary>
+        <Footer />
+      </Router>
+    </div>
   );
 }
 
